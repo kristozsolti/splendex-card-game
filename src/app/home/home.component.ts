16acdda040946eefdf361abcfeb68a4f2470b15d
kristozsolti <kristo.zsolti@gmail.com>
2021-04-07 15:34:37 +0300

    implementing the HOME feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  deckSize = 6;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  startGame(deckSize: number): void {
+    this.router.navigate(['/game']);
+  }
+
+  setDeckSize(deckSize: number): void {
+    this.deckSize = deckSize;
   }
 
 }
